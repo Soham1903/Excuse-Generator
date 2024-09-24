fd29e900d307caee7b83b5fd3628a96007b77703
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2024-09-24 23:45:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import "./App.css";
 
-import Todolist from "./components/Todolist";
-import Catfact from "./components/Catfact";
-// import "./CSS/Todolist.css";
-// import "./CSS/Catfact.css";
 import Excusegen from "./components/Excusegen";
 import "./CSS/Excusegen.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <Todolist /> */}
-      {/* <Catfact /> */}
       <Excusegen />
     </div>
   );
